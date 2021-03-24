107e15aa9e984cddcefa8c21ce5c7472326e279f
Saidali Saburov <ssaburov@velox.biz>
2021-03-24 23:34:13 +0500

    -updated card_test.go

--- pkg/bank/card/card_test.go
+++ pkg/bank/card/card_test.go
@@ -6,25 +6,41 @@ import (
 )
 
 func ExampleTotal() {
-	cards := []types.Card{
-		{
-			Balance: 1_000_00,
-			Active:  true,
-		},
-		{
-			Balance: 1_000_00,
-			Active:  true,
-		},
-		{
-			Balance: 1_000_00,
-			Active:  true,
-		},
-		{
-			Balance: 1_000_00,
-			Active:  true,
-		},
-	}
 
-	fmt.Println(Total(cards))
-	//Output: 100000
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: 1_000_00,
+			Active:  true,
+		},
+	}))
+
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: 1_000_00,
+			Active:  true,
+		},
+		{
+			Balance: 2_000_00,
+			Active:  true,
+		},
+	}))
+
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: 1_000_00,
+			Active:  false,
+		},
+	}))
+
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: -1_000_00,
+			Active:  true,
+		},
+	}))
+	//Output:
+	//100000
+	//300000
+	//0
+	//0
 }
